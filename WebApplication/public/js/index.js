4315853a6dca0a62f5597736b36048c1ad46c69c
PotatoMaster101 <PotatoMaster101@users.noreply.github.com>
2020-07-25 22:24:47 +0800

    Fixing button functions - Hotfix

--- WebApplication/public/js/index.js
+++ WebApplication/public/js/index.js
@@ -1,23 +1,23 @@
 function startSlideShow(){
-    let xhr = XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr,open('POST', "http://pi.phouse.local/internal/startslideshow", true);
     xhr.send();
 }
 
 function endSlideShow(){
-    let xhr = XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr,open('POST', "http://pi.phouse.local/internal/endslideshow", true);
     xhr.send();
 }
 
 function nextSlide(){
-    let xhr = XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr,open('POST', "http://pi.phouse.local/internal/nextslide", true);
     xhr.send();
 }
 
 function previousSlide(){
-    let xhr = XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr,open('POST', "http://pi.phouse.local/internal/previousslide", true);
     xhr.send();
 }
