98725ebc784e3ba3468a394f0e190287acacb70c
PotatoMaster101 <PotatoMaster101@users.noreply.github.com>
2020-07-25 22:31:19 +0800

    Fixing button functions - Hotfix

--- WebApplication/public/js/index.js
+++ WebApplication/public/js/index.js
@@ -2,22 +2,50 @@ function startSlideShow(){
     let xhr = new XMLHttpRequest();
     xhr.open('POST', "http://pi.phouse.local/internal/startslideshow", true);
     xhr.send();
+    xhr.addEventListener("readystatechange", processResponse, false);
+
+    function processResponse(e){
+        if(xhr.readyState == 4){
+            console.log(xhr.status);
+        }
+    }
 }
 
 function endSlideShow(){
     let xhr = new XMLHttpRequest();
     xhr.open('POST', "http://pi.phouse.local/internal/endslideshow", true);
     xhr.send();
+    xhr.addEventListener("readystatechange", processResponse, false);
+
+    function processResponse(e){
+        if(xhr.readyState == 4){
+            console.log(xhr.status);
+        }
+    }
 }
 
 function nextSlide(){
     let xhr = new XMLHttpRequest();
     xhr.open('POST', "http://pi.phouse.local/internal/nextslide", true);
     xhr.send();
+    xhr.addEventListener("readystatechange", processResponse, false);
+
+    function processResponse(e){
+        if(xhr.readyState == 4){
+            console.log(xhr.status);
+        }
+    }
 }
 
 function previousSlide(){
     let xhr = new XMLHttpRequest();
     xhr.open('POST', "http://pi.phouse.local/internal/previousslide", true);
     xhr.send();
+    xhr.addEventListener("readystatechange", processResponse, false);
+
+    function processResponse(e){
+        if(xhr.readyState == 4){
+            console.log(xhr.status);
+        }
+    }
 }
